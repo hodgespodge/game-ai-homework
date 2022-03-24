fab2cf33afe7366d55924efa3823fac7de2b59f4
hodgespodge <samuelhodges3000@gmail.com>
2022-03-23 23:58:49 -0400

    added manhattan heuristic and big graph

---
+++ homework-3/ManhattanHeuristic.h
@@ -0,0 +1,29 @@
+#ifndef MANHATTANHEURISTIC_H
+#define MANHATTANHEURISTIC_H
+
+# include "Heuristic.h"
+# include "GraphNode.h"
+# include <cmath>
+
+class ManhattanHeuristic: public Heuristic
+{
+public:
+    ManhattanHeuristic(GraphNode* goal) : Heuristic(goal) {
+    };
+
+    float f(GraphNode* node)
+    {
+
+        int x_diff = node->x - goal->x;
+        int y_diff = node->y - goal->y;
+
+        return abs(x_diff) + abs(y_diff);
+
+    }
+
+    ~ManhattanHeuristic()
+    {
+    }
+};
+
+#endif
